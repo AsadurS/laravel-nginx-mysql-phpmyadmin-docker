ccee458c787b42c51cecb0337e2c0d6c94eae771
Asadur <32617400+AsadurS@users.noreply.github.com>
2023-12-16 09:57:41 +0600

    established mysql connection and fetch data

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import {environment} from '../environments/environment'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  response :any;
+  $http = inject(HttpClient)
+  apiUrl = environment.appUrlApi
+  ngOnInit(){
+   this.$http.get(`${this.apiUrl}/test-route`).subscribe({
+    next:(response:any)=>{
+      this.response = response.data
+      console.log(this.response.data)
+    },error:(e)=> console.log(e)
+   })
+  }
 }
--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  appUrlApi: "http://localhost/api"
 };
